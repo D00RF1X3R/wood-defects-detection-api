f9ff89928cc3aa39d6c620b78d4c57237f68cc4b
D00RF1X3R <pismomaxaf@gmail.com>
2025-12-15 21:10:47 +0300

    Removed useless files

--- train.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-
-if __name__ == '__main__':
-    model = YOLO('yolo11s.pt')  # Load a pretrained nano model
-    model.train(data='dataset.yaml', epochs=25, batch=0.8, imgsz=640)  # Start training
